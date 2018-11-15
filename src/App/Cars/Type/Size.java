47bf7bf079d81dc1a34914065e95030db4f00034
Vladyslav Koniukhov <riggelllll@gmail.com>
2018-11-15 21:07:38 +0200

    Создан размер машин.

--- src/App/Cars/Type/Size.java
+++ src/App/Cars/Type/Size.java
@@ -1,4 +1,20 @@
 package App.Cars.Type;
 
 public enum Size {
+
+    SMALL(20), MIDDLE(50), LARGE(100);
+
+    private int size;
+
+    Size(int size){
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
