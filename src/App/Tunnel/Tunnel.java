d5efda30fcf8f125b1745e728a46d0b47fa72a7b
Vladyslav Koniukhov <riggelllll@gmail.com>
2018-11-15 22:23:04 +0200

    Заготовка тунеля.

--- src/App/Tunnel/Tunnel.java
+++ src/App/Tunnel/Tunnel.java
@@ -1,4 +1,21 @@
 package App.Tunnel;
 
+import App.Cars.Car;
+
+import java.util.ArrayDeque;
+
 public class Tunnel {
+
+    private boolean isEmpty;
+    private boolean isFull;
+    private static final int MAX_CARS_AMOUNT = 5;
+    private int carsCounter;
+    private ArrayDeque<Car> cars;
+
+    Tunnel(){
+        isEmpty = true;
+        isFull = false;
+        carsCounter = 0;
+        cars = new ArrayDeque<>();
+    }
 }
