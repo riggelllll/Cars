44e51ac24f04e8b253e790882a287841194e95fc
Vladyslav Koniukhov <riggelllll@gmail.com>
2018-11-15 21:43:19 +0200

    Создан класс машины.

--- src/App/Cars/Car.java
+++ src/App/Cars/Car.java
@@ -1,4 +1,38 @@
 package App.Cars;
 
+import App.Cars.Type.Size;
+import App.Cars.Type.Type;
+
 public class Car {
+    private Type type;
+    private Size size;
+    private int currentSize;
+    private int id;
+    private boolean isEmpty;
+
+    Car(Type type, Size size, int id){
+        this.type = type;
+        this.size = size;
+        this.id = id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
 }
