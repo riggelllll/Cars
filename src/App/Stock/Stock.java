27cdb86dda898403d460f8d7aa2ebca7ebb95cb1
Vladyslav Koniukhov <riggelllll@gmail.com>
2018-11-18 09:11:00 +0200

    Создана база класса.

--- src/App/Stock/Stock.java
+++ src/App/Stock/Stock.java
@@ -1,4 +1,34 @@
 package App.Stock;
 
+import App.Cars.Car;
+import App.Cars.Type.Type;
+
 public class Stock {
+    private Car car;
+    private String name;
+    private Type type;
+    private boolean isEmpty;
+
+    public Stock(String name, Type type) {
+        this.name = name;
+        this.type = type;
+        isEmpty = true;
+    }
+
+    public void setCar(Car car){
+        this.car = car;
+    }
+
+    public synchronized void loadCar() throws InterruptedException {
+        while (car.getCurrentSize() != car.getMaxsize()){
+            car.setCurrentSize(10);
+            Thread.sleep(1000);
+        }
+        System.out.println("Машина " + car.getType() + " " + car.getSize() +  " ЗАГРУЖЕНА и отправилась со склада");
+        car = null;
+    }
+
+
+
+
 }
